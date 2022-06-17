ad16f826e792b6a583d788042a88a1c1b9acd9c1
cy6erninja <cy6erninja@gmail.com>
2022-06-17 14:11:45 +0800

    Add empty EnglishAuction contract and tests.

---
+++ test/EnglishAuction.spec.ts
@@ -0,0 +1,23 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+import vuilder from '@vite/vuilder';
+import config from './vite.config.json';
+
+describe('English Auction works fine.', () => {
+
+    it('Test an auction can be created', async () => {
+
+    });
+
+    it('Test NFT is transfered on auction creation', async () => {
+
+    });
+
+    it('Test the highest bid can be sent to the contract', async () => {
+
+    });
+
+    it('Test a lower bid is returned to the owner', async () => {
+
+    });
+});
